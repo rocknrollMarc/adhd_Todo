cfe559887fc1cec48425bcc1682b58fc2ab0d5fd
Marc Bluemner <marc.bluemner@gmail.com>
2015-08-05 18:29:31 +0200

    can destiguish a completed task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+
 	it 'can destinguish a completed task' do
 		task = Task.new
 		expect(task).not_to be_complete
 		task.mark_complete
+		expect(task).to be_complete
 	end
+
+	
+	
 end
