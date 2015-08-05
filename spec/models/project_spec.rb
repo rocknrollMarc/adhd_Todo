5349a3498a7aa86960e7ec5b3a3689c5dd58b965
Marc Bluemner <marc.bluemner@gmail.com>
2015-08-05 18:38:44 +0200

    calculate extimates

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -20,6 +20,25 @@ RSpec.describe Project do
 			task.mark_complete
 			expect(project).to be_done
 		end
-		
 	end
+
+	describe 'estimates' do
+		let(:project) { Project.new	 }
+		let(:done) { Task.new(size: 2, completed: true) }
+		let(:small_not_done) { Task.new(size: 1) }
+		let(:large_not_done) { Task.new(size: 4) }
+
+		before(:example) do
+			project.tasks = [done, small_not_done, large_not_done]
+		end
+
+		it 'can calculate total size' do
+			expect(project.totalsize).to eq(7)
+		end
+
+		it 'can calculate remaining size' do
+			expect(project.remaining_size).to eq(5)
+		end
+	end
+
 end
